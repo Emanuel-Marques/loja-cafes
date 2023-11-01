051470bcf00d4040e962fa4e5fc775bec4a449ae
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2023-11-01 01:38:04 +0100

    Componente link

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <h1>Home page</h1>
+    <>
+      <h1>Coffeeteria</h1>
+      <Link to="/coffeeList">
+        <button>Conheça nossos produtos</button>
+      </Link>
+    </>
   )
 }
   
