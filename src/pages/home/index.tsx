ec151696d6e9e8270345b9e50e352fd21ee7288a
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2023-11-01 01:42:36 +0100

    Hook useNavigate: usando para mudar de rota, mais recomendado, melhor que o component Link

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,21 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
+  function handleClick(): void {
+    navigate('/coffeeList');
+  }
+
   return (
     <>
       <h1>Coffeeteria</h1>
-      <Link to="/coffeeList">
+      {/* Componente link */}
+      {/* <Link to="/coffeeList">
         <button>Conheça nossos produtos</button>
-      </Link>
+      </Link> */}
+      <button onClick={ handleClick } >Conheça nossos produtos</button>
     </>
   )
 }
